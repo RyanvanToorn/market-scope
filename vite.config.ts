859c63e427f6f24b034c91ed6ce82e5a151c5abe
DarkLineRSA <ryanvantoorn@gmail.com>
2026-02-03 20:16:37 +0200

    chore: addition of aliases for various src folders to allow for alias based imports in future code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@types': path.resolve(__dirname, './src/types'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@controllers': path.resolve(__dirname, './src/controllers'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
 })
