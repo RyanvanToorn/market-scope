e06dcffbd80c8238202127993b56b1bd44b4437b
DarkLineRSA <ryanvantoorn@gmail.com>
2026-02-04 22:42:23 +0200

    feat: addition of further types. Client specific interfaces and types will be kept in the same file as a rule to reduce bloat

--- src/interfaces/Listing.ts
+++ src/interfaces/Listing.ts
@@ -2,12 +2,12 @@ import type { AssetType } from "@type/asset-type";
 import type { Exchange } from "@type/exchange";
 import type { ListingStatus } from "@type/listing-status";
 
-export interface Listing{
-    symbol: string,
-    name: string,
-    exchange: Exchange,
-    assetType: AssetType,
-    ipoDate: Date,
-    delistingDate: Date | null,
-    status: ListingStatus
+export interface Listing {
+	symbol: string;
+	name: string;
+	exchange: Exchange;
+	assetType: AssetType;
+	ipoDate: Date;
+	delistingDate: Date | null;
+	status: ListingStatus;
 }
