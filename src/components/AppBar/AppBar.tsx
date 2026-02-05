edc7e3c135f4a3bb83fefc44d543c5a3cf1b63ce
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-05 20:02:09 +0200

    feat: added a variety of wrapped MUI components in a layered system should underlying library be changed in future

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,17 @@
+import type { StandardComponentProps } from '@interfaces/standard-component-props';
+import { AppBar as MUIAppBar } from '@mui/material';
+import styles from "./AppBar.module.css"
+
+
+export interface AppBarProps extends StandardComponentProps{
+    children?: React.ReactNode;
+}
+
+export function AppBar(props: AppBarProps){
+    return (
+        <MUIAppBar position='fixed' id={props.id} className={`${styles.AppBar} ${props.extendedClass}`} sx={props.sx}>
+            {props.children}
+        </MUIAppBar>
+    )
+}
+
