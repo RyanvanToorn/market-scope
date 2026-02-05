bd168994f3f79b5efd09ca2f81e28f032a53e24c
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-05 22:12:12 +0200

    refactor: created new seperate UI folder and relocated components + added layouts folder. Updated aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@type': path.resolve(__dirname, './src/types'),
-      '@interfaces': path.resolve(__dirname, './src/interfaces'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@controllers': path.resolve(__dirname, './src/controllers'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@type": path.resolve(__dirname, "./src/types"),
+			"@interfaces": path.resolve(__dirname, "./src/interfaces"),
+			"@services": path.resolve(__dirname, "./src/services"),
+			"@controllers": path.resolve(__dirname, "./src/controllers"),
+			"@components": path.resolve(__dirname, "./src/ui/components"),
+			"@layouts": path.resolve(__dirname, "./src/ui/layouts"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@styles": path.resolve(__dirname, "./src/styles"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
+		},
+	},
+});
