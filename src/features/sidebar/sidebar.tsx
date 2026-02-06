cac0f6f3fb8824a357eb37380b60eaf14bbe51a6
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-06 21:31:59 +0200

    feat: basic sidebar styling and initial implementation within BasicLayout

--- src/features/sidebar/sidebar.tsx
+++ src/features/sidebar/sidebar.tsx
@@ -1,20 +1,35 @@
 import { Box } from "@components/Box/Box";
 import styles from "./sidebar.module.css";
+import { Button } from "@components/Button/Button";
 
 export interface SidebarProps{
     isOpen: boolean;
+    onClose: ()=>void;
     headerContents?: React.ReactNode;
     bodyContents?: React.ReactNode;
 }
 
-export function Sidebar(): React.ReactElement | null{
-    return (
-        <Box className={styles.Sidebar}>
-            <Box className={styles.SidebarHeader}>
+export function Sidebar(props: SidebarProps): React.ReactElement | null{
 
-            </Box>
-            <Box className={styles.SidebarBody}>
-                
+    if(!props.isOpen){
+        return null
+    }
+
+
+    return (
+        <Box extendedClass={styles.SidebarContainer}>
+            <Box extendedClass={styles.Sidebar}>
+                <Box extendedClass={styles.SidebarHeader}>
+                    <Box extendedClass={styles.SidebarHeaderButtonBox}>
+                        <Button extendedClass="standard-button" onClick={props.onClose}>
+                            {"X"}
+                        </Button>
+                    </Box>
+                    {props.headerContents}
+                </Box>
+                <Box extendedClass={styles.SidebarBody}>
+                    {props.bodyContents}
+                </Box>
             </Box>
         </Box>
     );
--- src/ui/layouts/BasicLayout.tsx
+++ src/ui/layouts/BasicLayout.tsx
@@ -2,10 +2,21 @@ import { Box } from "@components/Box/Box";
 import styles from "./BasicLayout.module.css";
 import { AppBar } from "@components/AppBar/AppBar";
 import { APIButton } from "@features/api-button/api-button";
+import { Sidebar } from "@features/sidebar/sidebar";
+import { useState } from "react";
+
+
+
 
 
 
 export function BasicLayout(): React.ReactElement | null {
+    const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true);
+
+    const closeSidebar = () => {
+        setSidebarOpen(false);
+    }
+
     return (
         <Box extendedClass={styles.BasicLayout}>
             <Box extendedClass={styles.BasicLayoutTop}>
@@ -17,7 +28,7 @@ export function BasicLayout(): React.ReactElement | null {
                 <APIButton/>
             </Box>
             <Box extendedClass={styles.SidebarContainer}>
-
+                <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}/>
             </Box>
         </Box>
 
