064196a7d64f00286b28d25dcfe97f7be8995951
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-06 21:31:13 +0200

    feat: added onclick functionality and props for the button

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -5,11 +5,12 @@ import styles from './Button.module.css';
 export interface ButtonProps extends StandardComponentProps {
   children?: React.ReactNode;
   color?: 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+  onClick?:()=>void;
 }
 
 export function Button(props: ButtonProps): React.ReactElement | null {
   return (
-    <MUIButton id={props.id} className={`${styles.Button} ${props.extendedClass}`} sx={props.sx} color={props.color}>
+    <MUIButton id={props.id} className={`${styles.Button} ${props.extendedClass}`} onClick={props.onClick} sx={props.sx} color={props.color}>
       {props.children}
     </MUIButton>
   );
