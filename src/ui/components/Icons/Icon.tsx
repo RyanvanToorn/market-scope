4f05e0faf2edd16bbd4ee4bfd37a03f2a0ed0513
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-06 22:01:15 +0200

    refactor: updated Icon to use a prop for any MUI Icon or SVG

--- src/ui/components/Icons/Icon.tsx
+++ src/ui/components/Icons/Icon.tsx
@@ -1,15 +1,19 @@
 import type { StandardComponentProps } from '@interfaces/standard-component-props';
-import { Icon as MUIIcon } from '@mui/material';
+import { SvgIcon as MUIIcon} from '@mui/material/';
+import type { SvgIconProps } from '@mui/material/';
 import styles from './Icon.module.css';
 
 export interface IconProps extends StandardComponentProps {
-  children?: React.ReactNode;
+  icon: React.ElementType<SvgIconProps>;
 }
 
 export function Icon(props: IconProps): React.ReactElement | null {
   return (
-    <MUIIcon id={props.id} className={`${styles.Icon} ${props.extendedClass}`} sx={props.sx}>
-      {props.children}
-    </MUIIcon>
+    <MUIIcon
+      id={props.id}
+      component={props.icon}
+      className={`${styles.Icon} ${props.extendedClass}`}
+      sx={props.sx}
+    />
   );
 }
