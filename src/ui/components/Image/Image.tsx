168ec5f2f11f94094d6566e741b75dc205b7da42
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-02-09 21:01:16 +0200

    feat: added image component and further sidebar styling improvements

---
+++ src/ui/components/Image/Image.tsx
@@ -0,0 +1,17 @@
+import type { StandardComponentProps } from "@interfaces/standard-component-props";
+import styles from "./Image.module.css";
+
+export interface ImageProps extends StandardComponentProps{
+    url: string;
+    alt?: string;
+    width?: number;
+    height?: number;
+};
+
+export function Image(props: ImageProps): React.ReactElement | null {
+
+
+    return (
+        <img id={props.id} src={props.url} alt={props.alt} width={props.width? props.width : 600} height={props.height? props.height : 600} className={`${styles.Image} ${props.extendedClass ? props.extendedClass:''}`}/>
+    )
+}
--- src/ui/layouts/BasicLayout.tsx
+++ src/ui/layouts/BasicLayout.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@components/Box/Box";
 import styles from "./BasicLayout.module.css";
 import { AppBar } from "@components/AppBar/AppBar";
+import { Image } from "@components/Image/Image";
 import { APIButton } from "@features/api-button/api-button";
 import { Sidebar } from "@features/sidebar/sidebar";
 import { useState } from "react";
 import { Menu } from "@components/Menu/Menu";
+import logo from "@assets/Logo.png";
 
 
 
@@ -35,6 +37,7 @@ export function BasicLayout(): React.ReactElement | null {
             <Box extendedClass={styles.ContentContainer}>
                 <APIButton/>
             </Box>
+
             <Box extendedClass={styles.SidebarContainer}>
                 <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}/>
             </Box>
