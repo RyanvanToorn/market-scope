e3c4c7a940cf98608462dc4c55ce813b440cbea8
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-02-09 22:51:47 +0200

    feat: addition of MenuItem and Tab sub-components

---
+++ src/ui/components/Menu/MenuItem.tsx
@@ -0,0 +1,15 @@
+import type { StandardComponentProps } from "@interfaces/standard-component-props";
+import styles from "./MenuItem.module.css"
+import {MenuItem as MUIMenuItem} from '@mui/material/';
+
+export interface MenuItemProps extends StandardComponentProps{
+    contents?: React.ReactNode;
+}
+
+export function MenuItem(props: MenuItemProps): React.ReactElement | null  {
+    return (
+        <MUIMenuItem id={props.id} className={`${styles.MenuItem} ${props.extendedClass ?? ''}`} sx={props.sx}>
+            {props.contents}
+        </MUIMenuItem>
+    );
+}
---
+++ src/ui/components/Tabs/Tab.tsx
@@ -0,0 +1,15 @@
+import type { StandardComponentProps } from "@interfaces/standard-component-props";
+import { Tab as MUITab } from '@mui/material';
+import styles from "./Tab.module.css";
+
+export interface TabProps extends StandardComponentProps{
+    value: string;
+    label: string;
+    disabled?: boolean;
+}
+
+export function Tab(props: TabProps): React.ReactElement | null {
+    return (
+        <MUITab value={props.value} label={props.label} disabled={props.disabled} className={`${styles.Tab} ${props.extendedClass? props.extendedClass : ''}`}/>
+    );
+}
