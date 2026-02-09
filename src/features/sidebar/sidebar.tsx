11b3a3111f1f65b3f33b16eb9fedf8f844a425c1
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-09 15:24:20 +0200

    feat: added expanding sidebar logic and styling

--- src/features/sidebar/sidebar.tsx
+++ src/features/sidebar/sidebar.tsx
@@ -3,16 +3,32 @@ import styles from "./sidebar.module.css";
 import { Button } from "@components/Button/Button";
 import { Icon } from "@components/Icon/Icon";
 import CloseIcon from '@mui/icons-material/Close';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import { Paper } from "@components/Paper/Paper";
+import { useState } from "react";
 
 export interface SidebarProps{
     isOpen: boolean;
     onClose: ()=>void;
+    startExpanded?: boolean;
     headerContents?: React.ReactNode;
     bodyContents?: React.ReactNode;
 }
 
 export function Sidebar(props: SidebarProps): React.ReactElement | null{
+    let startExpanded = false;
+    if (!props.startExpanded){
+        startExpanded = false;
+    } else {
+        startExpanded = props.startExpanded;
+    }
+
+    const [isExpanded, setIsExpanded] = useState<boolean>(startExpanded);
+
+    function toggleExpand(){
+        setIsExpanded(!isExpanded);
+    }
 
     if(!props.isOpen){
         return null
@@ -20,11 +36,14 @@ export function Sidebar(props: SidebarProps): React.ReactElement | null{
 
 
     return (
-        <Box extendedClass={styles.SidebarContainer}>
+        <Box extendedClass={`${styles.SidebarContainer} ${isExpanded ? styles.SidebarContainerExpanded : ''}`}>
             <Box extendedClass={styles.Sidebar}>
                 <Box extendedClass={styles.SidebarHeader}>
-                    <Paper>
+                    <Paper extendedClass={styles.SidebarPaper}>
                         <Box extendedClass={styles.SidebarHeaderButtonBox}>
+                            <Button extendedClass="standard-button" onClick={toggleExpand} variant="contained">
+                                <Icon icon={isExpanded ? CloseFullscreenIcon : OpenInFullIcon}/>
+                            </Button>
                             <Button extendedClass="standard-button" onClick={props.onClose} variant="contained">
                                 <Icon icon={CloseIcon}/>
                             </Button>
@@ -33,7 +52,7 @@ export function Sidebar(props: SidebarProps): React.ReactElement | null{
                     </Paper>
                 </Box>
                 <Box extendedClass={styles.SidebarBody}>
-                    <Paper>
+                    <Paper extendedClass={styles.SidebarPaper}>
                         {props.bodyContents}
                     </Paper>
                 </Box>
