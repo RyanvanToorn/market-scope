967e71d7d655b6ecb8bcb11bb419cb26b0465a6f
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-10 08:34:51 +0200

    feat: further development on temporary links for views

--- src/ui/components/AppBar/AppBar.tsx
+++ src/ui/components/AppBar/AppBar.tsx
@@ -1,23 +1,29 @@
-import type { StandardComponentProps } from '@interfaces/standard-component-props';
-import { AppBar as MUIAppBar } from '@mui/material';
-import styles from "./AppBar.module.css"
+import type { StandardComponentProps } from "@interfaces/standard-component-props";
+import { AppBar as MUIAppBar, type SxProps, type Theme } from "@mui/material";
+import styles from "./AppBar.module.css";
 
-
-
-export interface AppBarProps extends StandardComponentProps{
-    children?: React.ReactNode;
+export interface AppBarProps extends StandardComponentProps {
+	children?: React.ReactNode;
 }
 
-export function AppBar(props: AppBarProps): React.ReactElement | null{
+export function AppBar(props: AppBarProps): React.ReactElement | null {
+	if (props.isVisible === false) {
+		return null;
+	}
 
-    if (props.isVisible === false){
-        return null;
-    }
-
-    return (
-        <MUIAppBar position='relative' id={props.id} className={`${styles.AppBar} ${props.extendedClass ?? ''}`} sx={props.sx}>
-            {props.children}
-        </MUIAppBar>
-    )
+	return (
+		<MUIAppBar
+			position="relative"
+			id={props.id}
+			className={`${styles.AppBar} ${props.extendedClass ?? ""}`}
+			sx={props.sx ? (Array.isArray(props.sx) ? [appBarSx, ...props.sx] : [appBarSx, props.sx]) : appBarSx}
+		>
+			{props.children}
+		</MUIAppBar>
+	);
 }
 
+const appBarSx: SxProps<Theme> = {
+	display: "flex",
+	flexDirection: "row",
+};
--- src/ui/layouts/BasicLayout.tsx
+++ src/ui/layouts/BasicLayout.tsx
@@ -1,71 +1,60 @@
 import { Box } from "@components/Box/Box";
 import styles from "./BasicLayout.module.css";
 import { AppBar } from "@components/AppBar/AppBar";
-import { Image } from "@components/Image/Image";
 import { APIButton } from "@features/api-button/api-button";
 import { Sidebar } from "@features/sidebar/sidebar";
 import { useState } from "react";
 import { Menu } from "@components/Menu/Menu";
-import logo from "@assets/Logo.png";
 import { Typography } from "@components/Typography/Typography";
 import { Link } from "@components/Link/Link";
 
-
-
-
-
-
 export function BasicLayout(): React.ReactElement | null {
-    const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true);
-    const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
+	const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true);
+	const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
 
-    const closeSidebar = () => {
-        setSidebarOpen(false);
-    }
+	const closeSidebar = () => {
+		setSidebarOpen(false);
+	};
 
-    const toggleMenu = () => {
-        setMenuOpen(!setMenuOpen);
-    }
+	const toggleMenu = () => {
+		setMenuOpen(!setMenuOpen);
+	};
 
-    return (
-        <Box extendedClass={styles.BasicLayout}>
-            <Box extendedClass={styles.BasicLayoutTop}>
-                <AppBar extendedClass={styles.AppBar}>
-                    <Menu isOpen={isMenuOpen}/>
-                    <Box extendedClass={styles.TitleContainer}>
-                        <Typography text={"Market Scope"} variant='h1' extendedClass={styles.Title} sx={titleSx}/>
-                    </Box>
+	return (
+		<Box extendedClass={styles.BasicLayout}>
+			<Box extendedClass={styles.BasicLayoutTop}>
+				<AppBar extendedClass={styles.AppBar}>
+					<Menu isOpen={isMenuOpen} />
+					<Box extendedClass={styles.TitleContainer}>
+						<Typography text={"Market Scope"} variant="h1" extendedClass={styles.Title} sx={titleSx} />
+					</Box>
 
-                    <Box extendedClass={styles.LinkContainer}>
-                        <Link href={"#"} extendedClass={styles.Link} contents={"test 1"}/>
-                        <Link href={"#"} extendedClass={styles.Link} contents={"test 2"}/>
-                        <Link href={"#"} extendedClass={styles.Link} contents={"test 3"}/>
-                        <Link href={"#"} extendedClass={styles.Link} contents={"test 4"}/>
-                    </Box>
-                    
-                </AppBar>
-            </Box>
+					<Box extendedClass={styles.LinkContainer}>
+						<Link href={"#"} extendedClass={styles.Link} contents={"Stocks"} />
+						<Link href={"#"} extendedClass={styles.Link} contents={"Crypto"} />
+						<Link href={"#"} extendedClass={styles.Link} contents={"Indices"} />
+					</Box>
+				</AppBar>
+			</Box>
 
-        <Box extendedClass={styles.BasicLayoutMiddle}>
-            <Box extendedClass={styles.ContentContainer}>
-                <APIButton/>
-            </Box>
+			<Box extendedClass={styles.BasicLayoutMiddle}>
+				<Box extendedClass={styles.ContentContainer}>
+					<APIButton />
+				</Box>
 
-            <Box extendedClass={styles.SidebarContainer}>
-                <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}/>
-            </Box>
-        </Box>
+				<Box extendedClass={styles.SidebarContainer}>
+					<Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+				</Box>
+			</Box>
 
-        <Box extendedClass={styles.BasicLayoutBottom}>
-            <Box extendedClass={styles.BottombarContainer}>
-
-            </Box>
-        </Box>
-        </Box>
-    )
+			<Box extendedClass={styles.BasicLayoutBottom}>
+				<Box extendedClass={styles.BottombarContainer}></Box>
+			</Box>
+		</Box>
+	);
 }
 
 const titleSx = {
-    fontSize: "2rem",
-    fontFamily: "'Fjalla One', sans-serif",
-}
+	fontSize: "2rem",
+	fontFamily: "'Fjalla One', sans-serif",
+};
