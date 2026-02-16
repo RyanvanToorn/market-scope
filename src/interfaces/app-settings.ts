e5d6c288d85db931527c453af76e5d68cd52fabd
Ryan van Toorn <ryanvantoorn@gmail.com>
2026-02-16 15:47:44 +0200

    feat: added initial types and utils for the addition of persistent browser storage

---
+++ src/interfaces/app-settings.ts
@@ -0,0 +1,9 @@
+import type { ThemeMode } from "@type/theme-mode";
+
+export interface AppSettings{
+    themeMode: ThemeMode;
+    apiKeys: {
+        alphaVantageKey?: string;
+        coinCapKey?: string;
+    }
+}
---
+++ src/utils/local-storage.ts
@@ -0,0 +1,37 @@
+export function safeGetItem(key: string): string {
+	if (typeof window === "undefined" || !("localStorage" in window)) {
+		return "";
+	}
+
+	try {
+		const value = window.localStorage.getItem(key);
+		return value ?? "";
+	} catch {
+		console.log("[LocalStorageHelper] failed to safely set item within local storage");
+        return "";
+	}
+}
+
+export function safeSetItem(key: string, value: string): void {
+	if (typeof window === "undefined" || !("localStorage" in window)) {
+		return;
+	}
+
+	try {
+		window.localStorage.setItem(key, value);
+	} catch {
+		console.log("[LocalStorageHelper] failed to safely set item within local storage");
+	}
+}
+
+export function safeRemoveItem(key: string): void {
+	if (typeof window === "undefined" || !("localStorage" in window)) {
+		return;
+	}
+
+	try {
+		window.localStorage.removeItem(key);
+	} catch {
+        console.log("[LocalStorageHelper] failed to safely remove item from local storage");
+	}
+}
