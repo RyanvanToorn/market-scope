d26a7aaa8866d46c0404bbb9c36976833642f3ec
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-02-16 18:47:26 +0200

    chore: added import of app.css into main.tsx for app specific themes/variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from '@screens/routes'
 import './index.css'
+import './app.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
