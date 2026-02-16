fd74edb1a8a3f7c3580ec10a247bbe10b748e18f
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-02-16 18:57:27 +0200

    chore: added props to text field component

--- src/ui/components/TextField/TextField.tsx
+++ src/ui/components/TextField/TextField.tsx
@@ -2,7 +2,19 @@ import type { StandardComponentProps } from '@interfaces/standard-component-prop
 import { TextField as MUITextField } from '@mui/material';
 import styles from './TextInput.module.css';
 
-export interface TextFieldProps extends StandardComponentProps {}
+export interface TextFieldProps extends StandardComponentProps {
+  onChange?: (event: object) => void;
+  autoFocus?: boolean;
+  defaultValue?: string;
+  disabled?: boolean;
+  error?: boolean;
+  fullWidth?: boolean;
+  placeholder?: string;
+  required?: boolean;
+  multiline?: boolean;
+  minRows?: number;
+  maxRows?: number;
+}
 
 export function TextField(props: TextFieldProps): React.ReactElement | null {
 
@@ -11,6 +23,9 @@ export function TextField(props: TextFieldProps): React.ReactElement | null {
     }
 
   return (
-    <MUITextField id={props.id} className={`${styles.TextField} ${props.extendedClass ?? ''}`} sx={props.sx} />
+    <MUITextField id={props.id} className={`${styles.TextField} ${props.extendedClass ?? ''}`} sx={props.sx} 
+    autoFocus={props.autoFocus} defaultValue={props.defaultValue} disabled={props.disabled} error={props.error}
+    fullWidth={props.fullWidth} placeholder={props.placeholder} required={props.required} multiline={props.multiline}
+    minRows={props.minRows} maxRows={props.maxRows} onChange={props.onChange}/>
   );
 }
