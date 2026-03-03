e121f0bfe9e7218637cef47896d42d0dc000d3d2
Ryan van Toorn <156212766+RyanvanToorn@users.noreply.github.com>
2026-03-03 19:32:55 +0200

    feat: added tanstack query wrapped calls for daily, weekly and monthly asset time series fetches

--- src/query/cached-api-controller-methods.ts
+++ src/query/cached-api-controller-methods.ts
@@ -1,11 +1,26 @@
 import { useAPIController } from "@context/APIControllerContext";
 import { useQuery } from "@tanstack/react-query";
+import type { AssetType } from "@type/asset-type";
+import type { ChartType } from "@type/chart-type";
 
-type CachedQueryOptions = {
+interface CachedQueryOptions {
 	enabled?: boolean;
 	staleTime?: number;
-};
+}
 
+interface AssetSpecificQueryOptions extends CachedQueryOptions {
+	assetSymbol: string;
+	assetType: AssetType;
+}
+
+const defaultStateTime = 24 * 60 * 1000;
+
+/** Util function to ensure keys are standardized */
+function buildQueryKey(assetSymbol: string, assetType: AssetType, chartType: ChartType): string {
+	return `${assetSymbol}-${assetType}-${chartType}`;
+}
+
+/** Fetch all listings - alpha vantage */
 export function useListingsQuery(options: CachedQueryOptions = {}) {
 	const api = useAPIController();
 
@@ -13,10 +28,11 @@ export function useListingsQuery(options: CachedQueryOptions = {}) {
 		queryKey: ["listings"],
 		queryFn: () => api.getAllSymbols(),
 		enabled: options.enabled ?? true,
-		staleTime: options.staleTime ?? 24 * 60 * 1000,
+		staleTime: options.staleTime ?? defaultStateTime,
 	});
 }
 
+/** Fetch all assets - coin cap */
 export function useAssetsQuery(options: CachedQueryOptions = {}) {
 	const api = useAPIController();
 
@@ -24,6 +40,48 @@ export function useAssetsQuery(options: CachedQueryOptions = {}) {
 		queryKey: ["assets"],
 		queryFn: () => api.getAssets(),
 		enabled: options.enabled ?? true,
-		staleTime: options.staleTime ?? 24 * 60 * 1000,
+		staleTime: options.staleTime ?? defaultStateTime,
+	});
+}
+
+/** Fetch daily data for equities - alpha vantage */
+export function useTimeSeriesDailyQuery(options: AssetSpecificQueryOptions) {
+	const api = useAPIController();
+
+	const key = buildQueryKey(options.assetSymbol, options.assetType, "Daily");
+
+	return useQuery({
+		queryKey: [key],
+		queryFn: () => api.getTimeSeriesDaily(options.assetSymbol),
+		enabled: options.enabled ?? true,
+		staleTime: options.staleTime ?? defaultStateTime,
+	});
+}
+
+/** Fetch weekly data for equities - alpha vantage */
+export function useTimeSeriesWeeklyQuery(options: AssetSpecificQueryOptions) {
+	const api = useAPIController();
+
+	const key = buildQueryKey(options.assetSymbol, options.assetType, "Weekly");
+
+	return useQuery({
+		queryKey: [key],
+		queryFn: () => api.getTimeSeriesWeekly(options.assetSymbol),
+		enabled: options.enabled ?? true,
+		staleTime: options.staleTime ?? defaultStateTime,
+	});
+}
+
+/** Fetch monthly data for equities - alpha vantage */
+export function useTimeSeriesMonthlyQuery(options: AssetSpecificQueryOptions) {
+	const api = useAPIController();
+
+	const key = buildQueryKey(options.assetSymbol, options.assetType, "Monthly");
+
+	return useQuery({
+		queryKey: [key],
+		queryFn: () => api.getTimeSeriesMonthly(options.assetSymbol),
+		enabled: options.enabled ?? true,
+		staleTime: options.staleTime ?? defaultStateTime,
 	});
 }
